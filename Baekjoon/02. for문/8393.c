6f0c505b5a8445bc3a362718c80b1ebf145f762c
Jihyun Kim <87523224+codehyunn@users.noreply.github.com>
2022-07-10 23:13:00 +0900

    create

---
+++ Baekjoon/02. for문/8393.c
@@ -0,0 +1,14 @@
+//8393 합
+#include <stdio.h>
+
+int main(void){
+    int n,i;
+    int sum=0;
+    scanf("%d", &n);
+
+    for (i=1; i<n+1; i++)
+        sum += i;
+    printf("%d",sum);
+    
+    return 0;
+}
