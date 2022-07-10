1583aba76a95be233e276c844dc953689a048eaa
Jihyun Kim <87523224+codehyunn@users.noreply.github.com>
2022-07-10 23:13:14 +0900

    create

---
+++ Baekjoon/02. for문/15552.c
@@ -0,0 +1,15 @@
+//15552 빠른 A+B
+#include <stdio.h>
+
+int main(void){
+    int t;
+    scanf("%d",&t);
+
+    for (int i=0;i<t;i++){
+        int a,b ;
+        scanf("%d %d",&a, &b);
+        printf("%d\n",a+b);
+    }
+
+    return 0;
+}
